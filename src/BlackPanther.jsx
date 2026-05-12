8098257226efe1b20f56b22e0128fdb26c7543d6
luca <luca.romano@t-online.de>
2026-05-12 13:29:31 +0200

    feat(reactRouter): :sparkles: utilize useNavigate to navigate back to main page

--- src/BlackPanther.jsx
+++ src/BlackPanther.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const BlackPanther = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    }
+
     return (
         <div>
             <h1>Black Panther</h1>
             <p>
                 Black Panther is a fictional character appearing in American comic books published by Marvel Comics. The character was created by writer-editor Stan Lee and artist-coplotter Jack Kirby, with the first appearance of the character in 'Fantastic Four' #52 (July 1966) and in the role of the Black Panther in 'Jungle Action' #6-9 (1973–1974).
             </p>
-            <button>To Heros</button>
+            <button onClick={handleClick}>To Heros</button>
         </div>
     );
 };
